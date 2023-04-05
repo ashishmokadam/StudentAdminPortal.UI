debdbd0c246d6549ec8910a72daf032dff3f1024
ashishmokadam <mokadamashish31@gmail.com>
2023-04-05 20:39:06 +0530

    Restructered models folder and updated student service

--- studentadminportal-ui/src/app/students/student.service.ts
+++ studentadminportal-ui/src/app/students/student.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Student } from '../api-models/student.model';
+import { Student } from '../models/api-models/student.model';
+
 
 @Injectable({
   providedIn: 'root'
